f8cac4175a61ccaeb9681d70692f48d715662be8
LucasAS07 <lucas-oliveira25@outlook.com>
2022-07-25 14:25:00 -0300

    Adicionado banco de dados

--- receitas_caseiras/settings.py
+++ receitas_caseiras/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'receitas_caseiras.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'receitas_caseira',
+        'USER': 'postgres',
+        'PASSWORD': '33715157',
+        'HOST': 'localhost'
     }
 }
 
